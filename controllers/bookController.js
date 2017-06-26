a098ca2d03f0a557c3a1a620350cb2676d2828e2
egin10 <egin066@gmail.com>
2017-06-26 18:37:32 +0800

    edit dikit

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,5 +1,5 @@
 var dataBooks = [
-	{ id : 1, judul : "Belajar HTML dan CSS", qty : 4},
+	{ id : 1, judul : "Belajar HTML dan CSS", qty : 2},
 	{ id : 2, judul : "Belajar OOP PHP", qty : 2},
 	{ id : 3, judul : "Belajar ExpressJs", qty : 1},
 ];
